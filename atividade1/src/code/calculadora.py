1a1b0f4307e81ec3057c673f59b79d89aebd826f
jonatasfelip <106175062+jonatasfelip@users.noreply.github.com>
2022-11-07 14:26:54 -0300

    primeiro commit

---
+++ atividade1/src/code/calculadora.py
@@ -0,0 +1,10 @@
+num1 = int(input('Número: '))
+num2 = int(input('Número: '))
+
+def Somar(num1, num2):
+    soma = num1 + num2
+    return soma
+
+print(Somar(num1, num2))
+
+#Documentação - docstrig
