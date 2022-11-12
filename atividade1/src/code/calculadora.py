28805340344607039764b51e53039c157c81bd5c
jonatasfelip <fjonatas237@gmail.com>
2022-11-11 21:36:55 -0300

    adicionando primeiros métodos

--- atividade1/src/code/calculadora.py
+++ atividade1/src/code/calculadora.py
@@ -1,10 +1,30 @@
-num1 = int(input('Número: '))
-num2 = int(input('Número: '))
+menu = """
+1...............Somar
+2...............Subtrair
+3...............Multiplicar
+4...............Dividir
+"""
 
-def Somar(num1, num2):
-    soma = num1 + num2
-    return soma
+print(menu)
 
-print(Somar(num1, num2))
+opc = int(input("Informe uma operação: "))
 
-#Documentação - docstrig
+num1 = int(input("Informe um número: "))
+num2 = int(input("Informe outro número: "))
+
+class Calculadora: 
+    """Classe calculadora que engloba métodos respectivos às operações: somar, subtrair, multiplicar e dividir"""
+    def Somar(num1, num2):
+        """Método de somar que recebe dois números e retorna a adição deles"""
+        soma = num1 + num2
+        return soma
+
+    def Subtrair(num1, num2): 
+        """Método de subtrair que recebe dois números e retorna a subtração deles"""
+        subtrair = num1 - num2
+        return subtrair
+
+    def Multiplicar(num1, num2): 
+        """Método de multiplicar que recebe dois números e retorna a multiplicação deles"""
+        multiplicar = num1 * num2
+        return multiplicar
